53d8b22d4f681cd3f0f45ec09ef5259e3641313b
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2023-07-08 18:23:04 +0200

    app1.py

--- app1.py
+++ app1.py
@@ -30,3 +30,7 @@ data_load_state = st.text('Loading data...')
 data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
+
+@st.cache_data
+def load_data(nrows):
+    data_load_state.text("Done! (using st.cache_data)")
