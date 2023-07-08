a883bf65a912ea10e14d02721edd4ec8072acef2
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2023-07-08 23:15:03 +0200

    app1.py

--- app1.py
+++ app1.py
@@ -31,7 +31,15 @@ data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
 
+#Histrogram
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
+
+#Plot data on a map
+st.subheader('Map of all pickups')
+st.map(data)
+st.subheader('Map of all pickups')
+st.map(data)
+
