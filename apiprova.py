63e8dd9d24ec83f4c005a0017626c080a57ad28f
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2023-07-09 09:12:24 +0200

    Add files via upload

---
+++ apiprova.py
@@ -0,0 +1,8 @@
+import streamlit as st
+header = st.beta_container()
+dataset = st.beta_container()
+features = st.beta_container()
+modelTraining = st.beta_container()
+
+with header:
+    st.Title("Welcome to my Data Science project")
