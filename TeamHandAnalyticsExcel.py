b3fd4e5aaf42af1278eb3e326eaf50057daec062
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2023-12-27 11:24:38 +0100

    TeamHandAnalyticsExcel.py

--- TeamHandAnalyticsExcel.py
+++ TeamHandAnalyticsExcel.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="HTA", layout="wide")
 def handle_action(team_name, rival_team, campo, phasegame, start, def_type, player, action_type, player2, sub_action_type, space, df):
     new_row = {'Team Name': team_name, 'Rival Team Name': rival_team, 'Lineup': campo, 'Phase Game': phasegame, 'Inici': start,
                'Def Type': def_type, 'Player': player, 'Action Type': action_type, 'Feeder': player2,'Sub Action': sub_action_type, 'Espai': space}
-    return pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+    return df.append(new_row, ignore_index=True)
 
 # Variable global para almacenar el estado del DataFrame
 if 'df' not in st.session_state:
