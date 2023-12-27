38a260b75bc5878dded350437489d892b965998a
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2023-12-27 11:32:40 +0100

    TeamHandAnalyticsExcel.py

--- TeamHandAnalyticsExcel.py
+++ TeamHandAnalyticsExcel.py
@@ -1,27 +1,30 @@
-import streamlit as st
+from transformers import pipeline
 import pandas as pd
+import streamlit as st
+import requests
 
 st.set_page_config(page_title="HTA", layout="wide")
 
-# Función para manejar las acciones y actualizar el DataFrame
 def handle_action(team_name, rival_team, campo, phasegame, start, def_type, player, action_type, player2, sub_action_type, space, df):
     new_row = {'Team Name': team_name, 'Rival Team Name': rival_team, 'Lineup': campo, 'Phase Game': phasegame, 'Inici': start,
                'Def Type': def_type, 'Player': player, 'Action Type': action_type, 'Feeder': player2,'Sub Action': sub_action_type, 'Espai': space}
-    return df.append(new_row, ignore_index=True)
+    df = df.append(new_row, ignore_index=True)
+    return df
 
-# Variable global para almacenar el estado del DataFrame
 if 'df' not in st.session_state:
     st.session_state.df = pd.DataFrame()
 
 col1, col2, col3, col4 = st.columns(4)
  
 with col1:
+
     # Interfaz de usuario con Streamlit
-    st.markdown('**HANDBALL TEAM ANALYSIS**')
+        st.markdown('**HANDBALL TEAM ANALYSIS**')
+
     # Pedir información inicial
-    team_name = st.text_input('Equipo')
-    rival_team = st.text_input('Rival')
-    campo = st.text_input('Jugadores a Pista')
+        team_name = st.text_input('Equipo')
+        rival_team = st.text_input('Rival')
+        campo = st.text_input('Jugadores a Pista')
 
 with col2:
 
@@ -33,6 +36,7 @@ with col2:
     # Desglosar tipos de acción y zonas en botones
     def_type = st.selectbox('**Tipo Defensa**', ('6:0','5:1','3:3','3:2:1', '4:2','Individual'))
 
+
 with col3:
 
     player = st.text_input('**Nº Jugador**')
@@ -40,8 +44,16 @@ with col3:
     player2 = st.text_input('**Nº Feeder**')
     sub_action_type = st.selectbox (':red[**Sub Acción**]', ('NA','Fijación','Asistencia','Desmarque sin balón'))
     
-
 with col4:
+
+    # Selectbox para seleccionar la opción de asistencia
+
+     # Espais Atacats
+    space = st.selectbox(
+        ':orange[Selecciona Espacio Atacado/Defendido]',
+        ('0-1', '1-2', '2-3', '3-3', '3-2', '2-1', '1-0', '7m', '9mIzquierda', '9mCentro', '9mDerecha', 'Otros'))
+    
+
     # Botón para registrar la acción
     if st.button('**Registrar Acción**'):
         st.session_state.df = handle_action(team_name, rival_team, campo, phasegame, start, def_type, player, action_type, player2, sub_action_type, space, st.session_state.df)
@@ -55,3 +67,4 @@ with col4:
     if st.button(':green[**Guardar en Excel**]'):
         st.session_state.df.to_excel('acciones_balonmano.xlsx', index=False)
         st.success('Datos guardados en acciones_balonmano.xlsx')
+
