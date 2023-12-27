6f299d3dd2922d0ec454a10d9d31a0f2a9eb11c9
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2023-12-27 12:51:16 +0100

    TeamHandAnalyticsExcel.py

--- TeamHandAnalyticsExcel.py
+++ TeamHandAnalyticsExcel.py
@@ -10,10 +10,6 @@ def handle_action(team_name, rival_team, campo, phasegame, start, def_type, play
     df = df.loc[len(df)] = new_row
     return df
 
-# Variable global para almacenar el estado del DataFrame
-if 'df' not in st.dfinal:
-    dfinal.df = pd.DataFrame()
-
 col1, col2, col3, col4 = st.columns(4)
  
 with col1:
