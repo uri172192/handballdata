6f89831cc09a0661291968100213aa9b25010870
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2023-12-27 12:36:39 +0100

    TeamHandAnalyticsExcel.py

--- TeamHandAnalyticsExcel.py
+++ TeamHandAnalyticsExcel.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="HTA", layout="wide")
 def handle_action(team_name, rival_team, campo, phasegame, start, def_type, player, action_type, player2, sub_action_type, space, df):
     new_row = {'Team Name': team_name, 'Rival Team Name': rival_team, 'Lineup': campo, 'Phase Game': phasegame, 'Inici': start,
                'Def Type': def_type, 'Player': player, 'Action Type': action_type, 'Feeder': player2,'Sub Action': sub_action_type, 'Espai': space}
-    df = df.append(new_row, ignore_index=True)
+    df = df.loc[len(df)] = new_row
     return df
 
 col1, col2, col3, col4 = st.columns(4)
