285f0a644f3f45b117f6aa635c2deca9051b35c4
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2024-01-01 22:36:24 +0100

    TeamHandAnalyticsGSDeploy.py

--- TeamHandAnalyticsGSDeploy.py
+++ TeamHandAnalyticsGSDeploy.py
@@ -25,8 +25,14 @@ if 'df' not in st.session_state:
 def handle_action(team_name, rival_team, campo, phasegame, start, def_type, player, action_type, player2, sub_action_type, space, df):
     new_row = {'Team Name': team_name, 'Rival Team Name': rival_team, 'Lineup': campo, 'Phase Game': phasegame, 'Inici': start,
                'Def Type': def_type, 'Player': player, 'Action Type': action_type, 'Feeder': player2,'Sub Action': sub_action_type, 'Espai': space}
-# Agrega una nueva fila al DataFrame utilizando el método loc
+    # Obtén la versión más actualizada del DataFrame
+    df = st.session_state.df
+
+    # Agrega una nueva fila al DataFrame utilizando el método loc
     df.loc[len(df)] = new_row
+    
+    # Actualiza el DataFrame en la variable de estado
+    st.session_state.df = df
     return df
 
 col1, col2, col3, col4 = st.columns(4)
