56fb817a7af1ba960e516442cc085ca3315823b7
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2024-01-01 21:06:15 +0100

    TeamHandAnalyticsGS.py

--- TeamHandAnalyticsGS.py
+++ TeamHandAnalyticsGS.py
@@ -20,10 +20,8 @@ st.set_page_config(page_title="HTA", layout="wide")
 def handle_action(team_name, rival_team, campo, phasegame, start, def_type, player, action_type, player2, sub_action_type, space, df):
     new_row = {'Team Name': team_name, 'Rival Team Name': rival_team, 'Lineup': campo, 'Phase Game': phasegame, 'Inici': start,
                'Def Type': def_type, 'Player': player, 'Action Type': action_type, 'Feeder': player2,'Sub Action': sub_action_type, 'Espai': space}
-     # Encuentra el índice del nuevo DataFrame
-    index = len(df)
-    # Asigna los valores al nuevo índice
-    df.loc[index] = new_row
+# Agrega una nueva fila al DataFrame utilizando el método loc
+    df.loc[len(df)] = new_row
     return df
 
 # Variable global para almacenar el estado del DataFrame
