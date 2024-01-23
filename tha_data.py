23ba636222f57308b95d6ac6ef027fdfa02c5f9a
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2024-01-23 14:33:54 +0100

    tha_data.py

--- tha_data.py
+++ tha_data.py
@@ -161,9 +161,7 @@ with col3:
     # Espais Atacats
     space = sac.segmented(items=
                               [sac.SegmentedItem(label='0-1'),
-                               sac.SegmentedItem(label='...', disabled=True),
-                               sac.SegmentedItem(label='7 metros'),
-                               sac.SegmentedItem(label='...', disabled=True),
+                               sac.SegmentedItem(label='-   7 metros   -'),
                                sac.SegmentedItem(label='1-0'),
                                sac.SegmentedItem(label='1-2'),
                                sac.SegmentedItem(label='2-3'),
