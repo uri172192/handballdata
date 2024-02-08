f93279b5014d6659dbee8b62b717bb87e9dff23f
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2024-02-08 11:24:51 +0100

    tha_data.py

--- tha_data.py
+++ tha_data.py
@@ -105,7 +105,7 @@ with col1:
                                sac.SegmentedItem(label='5:0'),
                                sac.SegmentedItem(label='4:0'),
                                sac.SegmentedItem(label='Individual')],
-                               label='**Tipo Defensa**', align='left', size='sm', divider=False)
+                               label='**Tipo Defensa**', align='left', size='sm', color='green', divider=False)
 with col2:
          
          if "page" not in st.session_state or st.session_state.page != "player_buttons":
